49a637284255bd7ae33d79c06d6387c4e660277d
62111341a <water.melon1341@gmail.com>
2022-02-07 10:28:59 +0900

    second-commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          validates :nickname, presence: true
          validates :profile, presence: true
-         validates :asign, presence: true
+         validates :assign, presence: true
          validates :position, presence: true
        
          has_many :prototypes
